8a601b77a90d69945c8a6652990a8236c41bbde7
Pavle Kraljic <kraljicpavle9@gmail.com>
2026-02-22 12:17:22 +0100

    Added onClick events and functions

--- quiz-app/pages/StartScreen.jsx
+++ quiz-app/pages/StartScreen.jsx
@@ -1,10 +1,26 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 function StartScreen() {
+  const [msg, setMsg] = useState("");
+
+  const navigate = useNavigate();
+
+  function startQuiz() {
+    navigate("/quiz");
+  }
+
+  function displayMsg() {
+    setMsg("Well to bad, click the start button to play");
+  }
+
   return (
     <div className="start-screen-container">
       <h1 className="start-screen-title">Welcome to the Quiz App!</h1>
       <p>Test your knowledge, click the button to start the quiz</p>
-      <button>Start</button>
-      <button>No</button>
+      <p>{msg}</p>
+      <button onClick={startQuiz}>Start</button>
+      <button onClick={displayMsg}>No</button>
     </div>
   );
 }
