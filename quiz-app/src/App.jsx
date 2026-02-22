c2f6a2e7fd72ba7d25cb7edbc7d222a6dfeda31e
Pavle Kraljic <kraljicpavle9@gmail.com>
2026-02-22 11:45:28 +0100

    Imported EndScreen and added to route

--- quiz-app/src/App.jsx
+++ quiz-app/src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import StartScreen from "../pages/StartScreen";
+import EndScreen from "../pages/EndScreen";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<StartScreen />} />
+      <Route path="/end-screen" element={<EndScreen />} />
     </Routes>
   );
 }
