2b57267dbd761e32faaca5a169c4948c11234849
Pavle Kraljic <kraljicpavle9@gmail.com>
2026-02-22 11:35:27 +0100

    Added content

---
+++ quiz-app/pages/StartScreen.jsx
@@ -0,0 +1,12 @@
+function StartScreen() {
+  return (
+    <div className="start-screen-container">
+      <h1 className="start-screen-title">Welcome to the Quiz App!</h1>
+      <p>Test your knowledge, click the button to start the quiz</p>
+      <button>Start</button>
+      <button>No</button>
+    </div>
+  );
+}
+
+export default StartScreen;
