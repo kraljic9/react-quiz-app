91fa02c54f1a8b5d3dd3795200a9722650ac8a54
Pavle Kraljic <kraljicpavle9@gmail.com>
2026-02-22 13:20:33 +0100

    used context to display points and imported questions to get the ammount of questions using length, added a restart function to reset the game and state

--- quiz-app/pages/EndScreen.jsx
+++ quiz-app/pages/EndScreen.jsx
@@ -1,15 +1,35 @@
+import { useContext } from "react";
+import { QuizContext } from "../context/QuizContext";
+import { questions } from "../data/Questions";
+import { useNavigate } from "react-router-dom";
+
 function EndScreen() {
+  let navigate = useNavigate();
+
+  function restart() {
+    setPoints(0);
+    setAnswerIndex(null);
+    setCurrentQuestionIndex(0);
+    navigate("/");
+  }
+
+  const { points, setPoints, setAnswerIndex, setCurrentQuestionIndex } =
+    useContext(QuizContext);
+
   return (
     <div className="end-screen-container">
       <div className="end-scree-text-container">
         <h1>Quiz completed</h1>
 
-        <p>Congratulations you answered 5/5 questions correctly!</p>
+        <p>
+          Congratulations you answered {points}/{questions.length} questions
+          correctly!
+        </p>
       </div>
 
       <div className="play-again-container">
         <p>Would you like to play again?</p>
-        <button>Restart</button>
+        <button onClick={restart}>Restart</button>
       </div>
     </div>
   );
